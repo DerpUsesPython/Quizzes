69db8c3d34616dbafa6f813420e92fc6fdd14533
DerpUsesPython <64509987+DerpUsesPython@users.noreply.github.com>
2020-04-28 19:44:44 -0700

    Add files via upload

---
+++ MathQuiz2.py
@@ -0,0 +1,95 @@
+print ('103+635=')
+Score = 0
+mathQ1 = int(input())
+if mathQ1 == 738:
+    print ('CORRECT')
+    Score = Score + 1
+else:
+    print ('Wrong!')
+    
+print ('100/2=')
+mathQ2 = int(input())
+if mathQ2 == 50:
+    print ('CORRECT')
+    Score = Score + 1
+else:
+    print ('Wrong!')
+
+print ('40/4=')
+mathQ3 = int(input())
+if mathQ3 == 10:
+    print ('CORRECT')
+    Score = Score + 1
+else:
+    print ('Wrong!')
+
+print ('1000+111=')
+mathQ4 = int(input())
+if mathQ4 == 1111:
+    print ('CORRECT')
+    Score = Score + 1
+else:
+    print ('Wrong!')
+
+print ('10x10x10')
+mathQ5 = int(input())
+if mathQ5 == 1000:
+    print ('CORRECT')
+    Score = Score + 1
+else:
+    print ('Wrong!')
+
+print ('44-4+404=')
+mathQ6 = int(input())
+if mathQ6 == 444:
+    print ('CORRECT')
+    Score = Score + 1
+else:
+    print ('Wrong!')
+    
+print ('14+200+31+50')
+mathQ7 = int(input())
+if mathQ7 == 295:
+    print ('CORRECT')
+    Score = Score + 1
+else:
+    print ('Wrong!')
+
+print ('5+5x5=')
+mathQ8 = int(input())
+if mathQ8 == 50:
+    print ('CORRECT')
+    Score = Score + 1
+else:
+    print ('Wrong!')
+
+print ('250/2')
+mathQ9 = int(input())
+if mathQ9 == 175:
+    print ('CORRECT')
+    Score = Score + 1
+else:
+    print ('Wrong!')
+
+print ('40-10+70')
+mathQ10 = int(input())
+if mathQ10 == 100:
+    print ('CORRECT')
+    Score = Score + 1
+else:
+    print ('Wrong!')
+
+print ('You have finished the test.')
+print ('Your score was',Score,'out of 10!')
+import time
+time.sleep(10) #delay for 10 seconds to see results.
+print ('bye')
+time.sleep(1)
+
+
+
+
+
+
+
+
